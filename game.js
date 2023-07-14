ac12c49b9889e89190068ac1bb36f5edb56841c9
SimPL3-PROG <121143564+SimPL3-PROG@users.noreply.github.com>
2023-07-14 21:14:30 +0530

    Update game.js

--- game.js
+++ game.js
@@ -6,27 +6,27 @@ var p2 = Math.round((Math.random()*5) + 1);
 
 if(p1==1)
 {
-    img1.setAttribute("src","dice1.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 else if (p1==2)
 {
-    img1.setAttribute("src","dice2.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 else if (p1==3)
 {
-    img1.setAttribute("src","dice3.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 else if (p1==4)
 {
-    img1.setAttribute("src","dice4.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 else if (p1==5)
 {
-    img1.setAttribute("src","dice5.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 else if (p1==6)
 {
-    img1.setAttribute("src","dice6.png");
+    img1.setAttribute("src","images\dice1.png");
 }
 
 if(p2==1)
